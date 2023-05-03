581cd0f28e6e117e6f1731b1b39e887fd38847aa
binhkid2 <binhkid2@gmail.com>
2023-05-03 23:01:04 +0700

    tee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Binh dep trai</h1>
+<style>
+    :global(h1) {
+        color:red;
+        animation: zoom 5s infinite;
+    }
+    @keyframes -global-zoom {
+        0%,100% {transform: scale(3);}
+        50% {transform: scale(0.5);}
+    }
+</style>
