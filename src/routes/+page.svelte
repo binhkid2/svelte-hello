49242970ae6e659b555401fae9abeeb4a8721f07
binhkid2 <binhkid2@gmail.com>
2023-05-05 12:28:51 +0700

    aa

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,80 @@
-<h1>Binh dep trai</h1>
-<style>
-    :global(h1) {
-        color:red;
-        animation: zoom 5s infinite;
+<script lang="ts">
+    import axios from "axios";
+    import { onMount, onDestroy, afterUpdate, beforeUpdate } from "svelte";
+    import { fade, fly } from "svelte/transition";
+  
+    let products: {
+        Description: string;
+        Thumbnail: string ;
+        Name:string
+    }[] = [];
+  
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://sheet.best/api/sheets/e056296f-e82d-43f3-a913-5b4d3ed0ad95"
+      );
+      products = response.data;
+    };
+  
+    onMount(() => {
+      fetchData();
+    });
+  
+   
+  
+ 
+  </script>
+  
+  <div class="container">
+   
+    <div class="photos">
+      {#each products as product, i (product.Name)}
+        <img
+          src={product.Thumbnail}
+          alt={product.Description}
+          class="image"
+          in:fly={{ y: 200, duration: 2000, delay: i * 200 }}
+          out:fade
+        />
+      {/each}
+    </div>
+  </div>
+  
+  <style>
+    .image {
+      width: 400px;
+      height: 250px;
+      margin: 5px;
     }
-    @keyframes -global-zoom {
-        0%,100% {transform: scale(3);}
-        50% {transform: scale(0.5);}
+    .photos {
+      display: flex;
+      flex-wrap: wrap;
     }
-</style>
+    .container {
+      width: 1230px;
+      margin: 0 auto;
+    }
+    .header {
+      text-align: center;
+      font-size: 20px;
+    }
+    .input {
+      padding: 10px;
+      width: 400px;
+      border-radius: 10px;
+      outline: none;
+      border: 1px solid gray;
+      font-size: 20px;
+    }
+    .button {
+      padding: 10px;
+      font-size: 20px;
+      background-color: aqua;
+      border-radius: 10px;
+      border: none;
+      color: white;
+    }
+    .input-container {
+      margin-bottom: 40px;
+    }
+  </style>
